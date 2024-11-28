da6ad8dbc1dc8c949a99fefd710449b7321a4de5
KABANGA David <Davkabanga96@gmail.com>
2024-11-28 20:45:02 +0200

    Implementing the Doctors component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -15,8 +15,8 @@ const Header = () => {
                 <p>Simply browse through our extensive list of trusted doctors, <br className='hidden sm:block' /> schedule your appointment hassle-free.
                 </p>
             </div>
-            <a className='flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300' href=''>
-                Book appointment <img src={assets.arrow_icon} alt='' />
+            <a className='flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300' href='#speciality'>
+                Book appointment <img className='w-3' src={assets.arrow_icon} alt='' />
             </a>
        </div>
 
